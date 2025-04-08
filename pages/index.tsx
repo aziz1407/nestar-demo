d9f53bf455105efe9e270f1ebc0cc955bd556c2e
aziz1407 <aibrokhimov2003@gmail.com>
2025-04-08 17:16:21 +0900

    feat: Organize and test pages router system

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,15 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Roboto_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
   subsets: ["latin"],
 });
 
@@ -22,9 +22,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div
-        className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-      >
+      <div className={`${styles.page} ${inter.variable} ${robotoMono.variable}`}>
+
         <main className={styles.main}>
           <Image
             className={styles.logo}
@@ -48,13 +47,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <Image
-                className={styles.logo}
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
+            <h1>PAGES ROUTER</h1>
               Deploy now
             </a>
             <a
